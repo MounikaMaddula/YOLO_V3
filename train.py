20c1cd2174067265fbc4dae86a6cc1d110cf6c55
Mounika <mounika2805@gmail.com>
2020-04-20 20:20:35 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,6 +5,8 @@ from torch.autograd import Variable
 import os
 import numpy as np
 
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 def train_model(train_dataloader, val_dataloader, model,criteria,optimizer,exp_lr_scheduler,epochs,start_epoch,out_dir):
     """
         Model training function
@@ -34,21 +36,24 @@ def train_model(train_dataloader, val_dataloader, model,criteria,optimizer,exp_l
             image,bnd_boxes, lables =  data
             bnd_boxes = bnd_boxes.squeeze(0) #M,4
             lables = lables.squeeze(0) #M
+            #image,bnd_boxes, lables = Variable(image.float()).to(device), Variable(bnd_boxes).to(device), Variable(lables).to(device)
             image,bnd_boxes, lables = Variable(image.float()), Variable(bnd_boxes), Variable(lables)
             predictions = model(image)
             _,loss = criteria(predictions, bnd_boxes,lables)
-            #net_loss += loss.data[0]/256
-            net_loss += loss.item()/256
+            net_loss += loss.data[0]
+            #net_loss += loss.item()/256
             loss.backward()
-            torch.nn.utils.clip_grad_norm(model.parameters(), 0.25)
+            #torch.nn.utils.clip_grad_norm(model.parameters(), 0.25)
             optimizer.step()
         exp_lr_scheduler.step()
 
         for param_group in optimizer.param_groups:
             print (param_group['lr'])
+        """
 
         for p in model.parameters():
             p.requires_grad = False
+
 
         model = model.eval()
 
@@ -57,18 +62,25 @@ def train_model(train_dataloader, val_dataloader, model,criteria,optimizer,exp_l
             image,bnd_boxes, lables =  data
             bnd_boxes = bnd_boxes.squeeze(0) #M,4
             lables = lables.squeeze(0) #M
+            #image,bnd_boxes, lables = Variable(image.float()).to(device), Variable(bnd_boxes).to(device), Variable(lables).to(device)
             image,bnd_boxes, lables = Variable(image.float()), Variable(bnd_boxes), Variable(lables)
+
             predictions = model(image)
             _,loss = criteria(predictions, bnd_boxes,lables)
             #net_loss += loss.data[0]/256
-            net_val_loss += loss.item()/256
+            net_val_loss += loss.item()/(9*bnd_boxes.shape[0])
 
         net_val_loss = net_val_loss/len(val_dataloader)
 
-        if net_val_loss < best_val_loss :
+        """
+        if net_loss < best_val_loss :
+            best_val_loss = net_loss
             torch.save(model.state_dict(),'best_chkpt.pth')
 
-        torch.save(model.state_dict(),'{0}/epoch-{1}.pth'.format(out_dir,epoch))
+        #torch.save(model.state_dict(),'{0}/epoch-{1}.pth'.format(out_dir,epoch))
+        
 
         print ('Epoch - {0} ---------> Loss - {1}'.format(epoch, net_loss/len(train_dataloader)))
         print ('#'*30)
+
+            
